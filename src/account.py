e2be19b678130aa461bb6e68bf8012a5c371c29f
=MaciejAzarewicz <=m.azarewicz.224@studms.ug.edu.pl>
2025-10-16 13:51:12 +0200

    zadania 2-5

--- src/account.py
+++ src/account.py
@@ -1,5 +1,22 @@
+def getpeseldate(pesel):
+    rok = int(pesel[0:2])
+    miesiac = int(pesel[2:4])
+    dzien = int(pesel[4:6])
+    if (miesiac > 20):
+        miesiac = miesiac - 20
+        rok = 2000 + rok
+    else:
+        rok = 1900 + rok
+    return [dzien,miesiac,rok]
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, pesel, kod):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0
+        if (len(str(pesel)) != 11):
+            self.pesel = "Invalid"
+        else:
+            self.pesel = pesel
+        kod = kod.split("_")
+        if(kod[0]=="PROM" and len(kod[1])==3 and getpeseldate(str(pesel))[2]>1960):
+            self.balance += 50
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -1,7 +1,18 @@
 from src.account import Account
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe","05240811968","PROM_123")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
-        assert account.balance == 0
+        assert account.balance == 50.0
+        assert account.pesel == "05240811968"
+        account2 = Account("","","123","PRO_123")
+        assert account2.balance == 0.0
+        assert account2.pesel == "Invalid"
+        account3 = Account("","","1234567891011","PROM123")
+        assert account3.pesel == "Invalid"
+        assert account3.balance == 0.0
+        account4 = Account("","","45042502666","PROM_123")
+        assert account4.balance == 0.0
+        account5 = Account("","",None,"")
+        assert account5.pesel == "Invalid"
